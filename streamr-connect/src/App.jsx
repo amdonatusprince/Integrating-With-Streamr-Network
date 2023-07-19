b27a9bca9ebda779e34ada7975ecefd769686c39
amdonatusprince <donatusprince@gmail.com>
2023-07-19 21:29:32 +0100

    setup: streamr chat screen

--- streamr-connect/src/App.jsx
+++ streamr-connect/src/App.jsx
@@ -5,25 +5,51 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  // Function to fetch messages (mocked API call)
+  const fetchMessages = async () => {
+    try {
+      const response = await axios.get(CHAT_SERVER_URL);
+      setMessages(response.data);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newMessage.trim() === "") return;
+    // Replace the following line with your API call to send messages to the chat server
+    // For this example, we're just appending the new message to the messages array.
+    setMessages([...messages, { text: newMessage, sender: "You" }]);
+    setNewMessage("");
+  };
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Received Messages</h1>
+      {messages.map((message, index) => (
+          <div key={index} className="message">
+            <strong>{message.sender}:</strong> {message.text}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
+      <h1>Streamr Chat</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <form onSubmit={handleSubmit} className="message-input-form">
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button type="submit">Send</button>
+      </form>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
